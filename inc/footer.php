aaa3cd563b56d5ea58d18ace9e200ca5313dd4ec
Alister Animesh Baroi <44337842+AlisterBaroi@users.noreply.github.com>
2021-08-13 22:20:52 +0800

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 <footer>
 <div class="text-center py-2 bg-secondary text-light fixed-bottom">
-    &copy; 2021 <a class="text-light" href="index.php">Ecommerce Store</a>
+    &copy; <?php echo date("Y"); ?> <a class="text-light" href="index.php">Ecommerce Store</a>
   </div></footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/113654cb35.js" crossorigin="anonymous"></script>
